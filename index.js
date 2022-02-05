26925df567ac2ec69b63797649fa90f2e29ec731
ArSaN <arthnardini24@gmail.com>
2022-02-05 12:56:46 -0300

    Bugfix/Fixed-package.json

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const path = require('path')
 
 const app = express();
+
 app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.get("/fruits", cors(), async (req, res, next) => {
